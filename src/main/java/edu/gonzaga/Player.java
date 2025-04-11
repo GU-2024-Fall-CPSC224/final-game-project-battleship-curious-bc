7a09f7b1cd4f70046bb50b9f395b375835531e14
Usanomi <163929792+Usanomi@users.noreply.github.com>
2025-04-11 12:45:32 -0700

    Player deetails

--- src/main/java/edu/gonzaga/Player.java
+++ src/main/java/edu/gonzaga/Player.java
@@ -1,1 +1,29 @@
-//test
+package edu.gonzaga;
+
+public class Player {
+
+    protected String name;
+    protected int score;
+
+    public Player(String name) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore(int score) {
+        return score;
+    }
+
+    public void setScore(int score){
+        this.score = score;
+    }
+
+}
